93aa0b7d28375f4813164f2ca17e9b8966df7afd
Arslan Sadiq <arslanmsadiq@gmail.com>
2016-06-05 23:03:36 +0000

    Added a navigation bar to the top of the webpage

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+#For all the code in /pages
+#back-end code
+class PagesController < ApplicationController
+  def home
+  end
+
+  def index
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+end
